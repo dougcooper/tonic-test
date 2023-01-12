a45d60f32860757e82b02d893e2491d9b1e2d18a
Doug Cooper <douglas.cooper1@gmail.com>
2023-01-12 17:47:55 -0500

    make streaming server greet

--- examples/src/streaming/server.rs
+++ examples/src/streaming/server.rs
@@ -1,62 +1,38 @@
 pub mod pb {
-    tonic::include_proto!("grpc.examples.echo");
+    tonic::include_proto!("greet");
 }
 
 use futures::Stream;
-use std::{error::Error, io::ErrorKind, net::ToSocketAddrs, pin::Pin, time::Duration};
+use std::{net::ToSocketAddrs, pin::Pin, time::Duration};
 use tokio::sync::mpsc;
 use tokio_stream::{wrappers::ReceiverStream, StreamExt};
-use tonic::{transport::Server, Request, Response, Status, Streaming};
+use tonic::{transport::Server, Request, Response, Status};
+use tokio::time::{sleep};
 
-use pb::{EchoRequest, EchoResponse};
+use crate::pb::{HelloRequest,HelloReply};
+use crate::pb::greeter_server::GreeterServer;
 
-type EchoResult<T> = Result<Response<T>, Status>;
-type ResponseStream = Pin<Box<dyn Stream<Item = Result<EchoResponse, Status>> + Send>>;
-
-fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
-    let mut err: &(dyn Error + 'static) = err_status;
-
-    loop {
-        if let Some(io_err) = err.downcast_ref::<std::io::Error>() {
-            return Some(io_err);
-        }
-
-        // h2::Error do not expose std::io::Error with `source()`
-        // https://github.com/hyperium/h2/pull/462
-        if let Some(h2_err) = err.downcast_ref::<h2::Error>() {
-            if let Some(io_err) = h2_err.get_io() {
-                return Some(io_err);
-            }
-        }
-
-        err = match err.source() {
-            Some(err) => err,
-            None => return None,
-        };
-    }
-}
+type HelloResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<HelloReply, Status>> + Send>>;
 
 #[derive(Debug)]
-pub struct EchoServer {}
+pub struct HelloServer {}
 
 #[tonic::async_trait]
-impl pb::echo_server::Echo for EchoServer {
-    async fn unary_echo(&self, _: Request<EchoRequest>) -> EchoResult<EchoResponse> {
-        Err(Status::unimplemented("not implemented"))
-    }
+impl pb::greeter_server::Greeter for HelloServer {
 
-    type ServerStreamingEchoStream = ResponseStream;
+    type SayHelloStream = ResponseStream;
 
-    async fn server_streaming_echo(
+    async fn say_hello(
         &self,
-        req: Request<EchoRequest>,
-    ) -> EchoResult<Self::ServerStreamingEchoStream> {
-        println!("EchoServer::server_streaming_echo");
+        req: Request<HelloRequest>,
+    ) -> HelloResult<Self::SayHelloStream> {
+        println!("HelloServer::say_hello");
         println!("\tclient connected from: {:?}", req.remote_addr());
 
         // creating infinite stream with requested message
-        let repeat = std::iter::repeat(EchoResponse {
-            message: req.into_inner().message,
+        let repeat = std::iter::repeat(HelloReply{
+            message: req.into_inner().name,
         });
         let mut stream = Box::pin(tokio_stream::iter(repeat).throttle(Duration::from_millis(200)));
 
@@ -64,6 +40,7 @@ impl pb::echo_server::Echo for EchoServer {
         // the `out_stream` will not be polled after client disconnect
         let (tx, rx) = mpsc::channel(128);
         tokio::spawn(async move {
+            sleep(Duration::from_secs(3600)).await;
             while let Some(item) = stream.next().await {
                 match tx.send(Result::<_, Status>::Ok(item)).await {
                     Ok(_) => {
@@ -80,74 +57,17 @@ impl pb::echo_server::Echo for EchoServer {
 
         let output_stream = ReceiverStream::new(rx);
         Ok(Response::new(
-            Box::pin(output_stream) as Self::ServerStreamingEchoStream
-        ))
-    }
-
-    async fn client_streaming_echo(
-        &self,
-        _: Request<Streaming<EchoRequest>>,
-    ) -> EchoResult<EchoResponse> {
-        Err(Status::unimplemented("not implemented"))
-    }
-
-    type BidirectionalStreamingEchoStream = ResponseStream;
-
-    async fn bidirectional_streaming_echo(
-        &self,
-        req: Request<Streaming<EchoRequest>>,
-    ) -> EchoResult<Self::BidirectionalStreamingEchoStream> {
-        println!("EchoServer::bidirectional_streaming_echo");
-
-        let mut in_stream = req.into_inner();
-        let (tx, rx) = mpsc::channel(128);
-
-        // this spawn here is required if you want to handle connection error.
-        // If we just map `in_stream` and write it back as `out_stream` the `out_stream`
-        // will be drooped when connection error occurs and error will never be propagated
-        // to mapped version of `in_stream`.
-        tokio::spawn(async move {
-            while let Some(result) = in_stream.next().await {
-                match result {
-                    Ok(v) => tx
-                        .send(Ok(EchoResponse { message: v.message }))
-                        .await
-                        .expect("working rx"),
-                    Err(err) => {
-                        if let Some(io_err) = match_for_io_error(&err) {
-                            if io_err.kind() == ErrorKind::BrokenPipe {
-                                // here you can handle special case when client
-                                // disconnected in unexpected way
-                                eprintln!("\tclient disconnected: broken pipe");
-                                break;
-                            }
-                        }
-
-                        match tx.send(Err(err)).await {
-                            Ok(_) => (),
-                            Err(_err) => break, // response was droped
-                        }
-                    }
-                }
-            }
-            println!("\tstream ended");
-        });
-
-        // echo just write the same data that was received
-        let out_stream = ReceiverStream::new(rx);
-
-        Ok(Response::new(
-            Box::pin(out_stream) as Self::BidirectionalStreamingEchoStream
+            Box::pin(output_stream) as Self::SayHelloStream
         ))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let server = EchoServer {};
+    let server = HelloServer {};
     Server::builder()
-        .add_service(pb::echo_server::EchoServer::new(server))
-        .serve("[::1]:50051".to_socket_addrs().unwrap().next().unwrap())
+        .add_service(GreeterServer::new(server))
+        .serve("[::1]:5001".to_socket_addrs().unwrap().next().unwrap())
         .await
         .unwrap();
 
